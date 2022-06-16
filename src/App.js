711a7dd0f69b2ec783c9a5e94799706666718a8c
John Bee <john.barra@hotmail.co.uk>
2022-06-16 13:34:57 +0100

    Basic Landing page
    Created Basic Landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Repolist } from "./components/repo-list";
+import { Scene } from "./components/background.js";
+import GHlogo from "./images/GitHub_Logo_White.png";
 
 function App() {
-  return (
+    let reactList = [
+        {
+            name: "Word Blitz",
+            link: "https://johnbee.github.io/word-blitz/",
+            repo: "https://github.com/JohnBee/word-blitz"
+        },
+        {
+            name: "React Snake",
+            link: "https://johnbee.github.io/ReactSnake/",
+            repo: "https://github.com/JohnBee/ReactSnake"
+        }
+    ]
+    let webglList = [
+        {
+            name: "Sierpinksi 3D",
+            link: "https://johnbee.github.io/3d-sierpinski/",
+            repo: "https://github.com/JohnBee/3d-sierpinski",
+        },
+    ]
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <Scene /> */}
+        <h1 className="Header1">
+        John Barraclough
+        </h1>
+        <div className="GitHubLogoDiv">
+            <a className="GHLogoLink" href="https://github.com/JohnBee">
+                <img className="GHlogo" src={GHlogo} alt="Link to GitHub"></img>
+            </a>
+        </div>
+        <h2 className="Header2">
+            React JS
+        </h2>
+        <Repolist items={reactList}/>
+        <h2 className="Header2">
+            WebGL ThreeJS
+        </h2>
+        <Repolist items={webglList} />
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/repo-list.js
@@ -0,0 +1,33 @@
+import GHmark from "../images/GitHub-Mark-Light-120px-plus.png";
+function ListItem(props){
+    return(
+        <tbody>
+            <tr>
+                <td className="ListDiv">
+                    <a className="ListLink" href={props.link}>
+                        {props.name}
+                    </a>
+                </td>
+                <td>
+                    <a href={props.repo}>
+                        <img className="ListGHmark" src={GHmark} alt="GHmark"></img>
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+    );
+}
+
+export function Repolist(props){
+    return (
+        <div className="Repolist">
+            <table className="ItemList">
+                {
+                    props.items.map((x, index) =>
+                            <ListItem key={index} name={x.name} link={x.link} repo={x.repo}/>
+                    )
+                }
+            </table>
+        </div>
+    );
+}
