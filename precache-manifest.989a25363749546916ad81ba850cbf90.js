c3ba3adfdbdc10dba641c997d49eb83d6f5a7848
John Bee <john.barra@hotmail.co.uk>
2022-07-06 20:08:07 +0100

    Updates

---
+++ precache-manifest.989a25363749546916ad81ba850cbf90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d881301599c98e1fe5e",
+    "url": "/landing-page/static/css/main.6bd9e069.chunk.css"
+  },
+  {
+    "revision": "5d881301599c98e1fe5e",
+    "url": "/landing-page/static/js/main.5d881301.chunk.js"
+  },
+  {
+    "revision": "c7942216d879405da885",
+    "url": "/landing-page/static/js/1.c7942216.chunk.js"
+  },
+  {
+    "revision": "32f7df91e4b1c3a7b280",
+    "url": "/landing-page/static/js/2.32f7df91.chunk.js"
+  },
+  {
+    "revision": "f127c2a95f36f5952c9c",
+    "url": "/landing-page/static/js/runtime~main.f127c2a9.js"
+  },
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/landing-page/static/media/GitHub_Logo_White.064ace47.png"
+  },
+  {
+    "revision": "dedfef944307aa8a454ea75e683fe5ae",
+    "url": "/landing-page/index.html"
+  }
+];
